96075b84d948a8da07bbf1108086bbda0906f4cb
Mariam <mariam.yasser772000@gmail.com>
2023-11-14 13:18:21 -0800

    1

---
+++ print_un1.c
@@ -0,0 +1,102 @@
+#include "main.h"
+/**
+ * unsig_hex - Convert an unsigned integer to a hexadecimal
+ * @num: Unsigned integer
+ * @specifier: Conversion specifier ('x' or 'X')
+ *
+ * Return: The hexadecimal string representation of the number
+ */
+
+char *unsig_hex(unsigned int num, char specifier)
+{
+int len = _num_digits_hex(num);
+
+char *hex_str = malloc(len + 1);
+
+int remainder
+
+if (hex_str == NULL)
+	return (NULL);
+
+hex_str[len] = '\0';
+
+while (len > 0)
+{
+	len--;
+	remainder = num % 16;
+	if (remainder < 10)
+		hex_str[len] = '0' + remainder;
+	else
+		hex_str[len] = specifier == 'x' ? 'a' + remainder -10
+			: 'A' + remainder -10;
+	num /= 16;
+}
+
+return (hex_str);
+}
+
+/**
+ * _num_digits_hex - count the number of digits in hexadecimal
+ * @num: Unsigned integer
+ *
+ * Return:  the number of digits
+ */
+
+int _num_digits_hex(unsigned int num)
+{
+int count = 0;
+
+if (num == 0)
+	return (1);
+while (num > 0)
+{
+	num /= 16;
+	count++;
+}
+return (count);
+}
+
+
+/**
+ * unsig - Convert an unsigned integer
+ * @num: Unsigned integer
+ * @base: Base for conversion (8 for octal, 10 for decimal)
+ *
+ * Return: The string representation of the number
+ */
+char *unsig(unsigned int num, int base)
+{
+int len;
+unsigned int temp;
+char *str;
+
+if (num == 0)
+{
+	str = malloc(2);
+	if (str == NULL)
+		return (NULL);
+	str[0] = '0';
+	str[1] = '\0';
+	return (str);
+}
+len = 0;
+temp = num;
+while (temp > 0)
+{
+temp /= base;
+len++;
+}
+str = malloc(len + 1);
+if (str == NULL)
+	return (NULL);
+
+str[len] = '\0';
+while (len > 0)
+{
+len--;
+str[len] = '0' + num % base;
+num /= base;
+}
+return (str);
+}
+
---
+++ print_un2.c
@@ -0,0 +1,72 @@
+/**
+ * _strlen - Calculate the length of a string
+ * @str: pointer to string
+ *
+ * Return: The length of the string
+ */
+int _strlen(const char *str)
+{
+int len;
+
+len = 0;
+while (*str)
+{
+len++;
+str++;
+}
+return (len);
+}
+
+
+/**
+ * _num_digits - count the number of digits in an integer
+ * @num: the integer number
+ *
+ * Return: the count number of digits in the integer
+ */
+int _num_digits(unsigned int num)
+{
+int count;
+
+count = 0;
+if (num == 0)
+return (1);
+
+while (num > 0)
+{
+num /= 10;
+count++;
+}
+
+return (count);
+}
+
+/**
+ * print_number - Print an unsigned integer in the specified format
+ * @args: va_list argument
+ * @specifier: Conversion specifier ('u', 'o', 'x', or 'X')
+ *
+ * Return: Number of characters printed
+ */
+int print_number(va_list args, char specifier)
+{
+unsigned int num;
+char *str;
+
+num = va_arg(args, unsigned int);
+
+if (specifier == 'u')
+str = unsig(num, 10);
+else if (specifier == 'o')
+str = unsig(num, 8);
+else if (specifier == 'x' || specifier == 'X')
+str = unsig_hex(num, specifier);
+
+if (str == NULL)
+return (0);
+
+write(1, str, _strlen(str));
+free(str);
+
+return (_num_digits(num));
+}
