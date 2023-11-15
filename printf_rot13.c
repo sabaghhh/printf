066d397f6d1f23210beb9eb0ad337322b98da612
Mariam <mariam.yasser772000@gmail.com>
2023-11-14 17:52:20 -0800

    1

--- printf_rot13.c
+++ printf_rot13.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * printf_rot13 - encode dtring with rot13
+ * print_rot13 - encode dtring with rot13
  * @args:  list of arguments
  * @buffer: buffer
  * @buf_ptr: buffer pointer
  *
  * Return: number of characters printed
  */
-int printf_rot13(va_list args, char *buffer, char **buf_ptr)
+int print_rot13(va_list args, char *buffer, char **buf_ptr)
 {
 int count;
 char base, c;
