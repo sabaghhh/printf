9f1d71e0f1820edc21fef0a492b1b8dbb528a936
Mariam <mariam.yasser772000@gmail.com>
2023-11-14 18:11:46 -0800

    1

--- print_rot13.c
+++ print_rot13.c
@@ -5,7 +5,7 @@
  * @buffer: buffer
  * @buf_ptr: buffer pointer
  *
- * Return: number of characters printed
+ * Return: count which is number of characters printed
  */
 int print_rot13(va_list args, char *buffer, char **buf_ptr)
 {
